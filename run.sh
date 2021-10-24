4688b182816c86b89aa43895e9ec17fa58a54226
Jeremy Elam <me@jeremyelam.net>
2021-10-24 02:15:05 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ eval $(minikube docker-env)
 docker build . -t frontdoor-elam:v1
 kubectl apply -f frontdoor-elam.yaml
 echo "Sleeping 5 seconds to wait for app deploy"
+sleep 5
+unset url
 url=$(minikube service frontdoor-elam-svc --url)
 curl -d {"Name":"test"} -H "Content-Type: application/json" $url
