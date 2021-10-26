3226e33ae219c1ff328ca8c8716b127de1992b67
Jeremy Elam <me@jeremyelam.net>
2021-10-26 16:58:52 -0500

    updated

--- frontdoor.go
+++ frontdoor.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 )
@@ -11,6 +12,14 @@ func handle(rw http.ResponseWriter, req *http.Request) {
 	for name, headers := range req.Header {
 		for _, h := range headers {
 			fmt.Fprintf(rw, "%v: %v\n", name, h)
+		}
+	}
+
+	host, _ := os.Hostname()
+	addrs, _ := net.LookupIP(host)
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			fmt.Fprintln(rw, "IPv4: ", ipv4)
 		}
 	}
 	// Echo the body back
